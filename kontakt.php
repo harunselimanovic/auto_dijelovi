0b06f287741478b7c3ebb57d6960a9282fec0fbf
ErminDedic <ermyn.dedic@gmail.com>
2019-06-16 22:10:16 +0200

    Kontakt

--- kontakt.php
+++ kontakt.php
@@ -3,8 +3,13 @@ include('header.html');
 ?>
 
     <div class="row">
-
-Kontakt
+Kontakt podaci uposlenika: 
+    <h3>Samira Bišić, generalni direktor</h3>
+    <p>samira.bisic@gmail.com</p>
+    <h3>Harun Selimanović, tehnički direktor</h3>
+    <p>harun.selimanovic@gmail.com</p>
+    <h3>Ermin Dedić, finansijski direktor</h3>
+    <p>ermyn.dedic@gmail.com</p>
     </div>
 
 <?php
